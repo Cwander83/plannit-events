d2d6a181120a0565ea1d1ff58bbe8900f7b30e11
Chris Wander <31929558+Cwander83@users.noreply.github.com>
2021-01-17 16:31:57 -0500

    form style done

--- src/pages/PartnerForm.jsx
+++ src/pages/PartnerForm.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
-// import Image from '../images/pic-food-1.jpg';
-// import Image2 from '../images/pic-dancing-1.jpg';
-// import Image3 from '../images/pic-dj-1.jpg';
-
 const PartnerForm = () => {
 	return (
 		<div className="flex flex-col justify-center">
@@ -11,11 +7,11 @@ const PartnerForm = () => {
 				<h1 className="w-full text-lg lg:text-5xl font-semibold font-title text-center mt-7">
 					Partnership Contact Form
 				</h1>
-				<h1 className="w-full text-lg lg:text-2xl font-semibold  text-center mt-7">
-					Look Forward to Working With You!
+				<h1 className=" w-4/6 mx-auto text-lg lg:text-2xl font-medium  text-center mt-7">
+					Get Started by telling us about your business.
 				</h1>
 			</div>
-			<div className="text-center">
+			<div className="text-center mb-6">
 				<div className=" w-11/12 mx-auto">
 					<form
 						className="w-full  mt-6 mx-auto max-w-4xl border-secondary border-2 p-6 sm:p-8 flex flex-wrap"
@@ -35,7 +31,7 @@ const PartnerForm = () => {
 								name="name"
 								id="name"
 								autoComplete="name"
-								className="mt-1 focus:border-primary focus:ring-0 focus:border-3 focus:text-xl block w-full  text-center text-xl tracking-wider border-gray-light border-t-0 border-l-0 border-r-0"
+								className="mt-1 focus:border-primary focus:ring-0 focus:border-3 focus:text-xl block w-full  text-center text-xl tracking-wider border-gray-dark border-t-0 border-l-0 border-r-0"
 							/>
 						</div>
 
@@ -52,11 +48,11 @@ const PartnerForm = () => {
 								name="business_name"
 								id="business_name"
 								autoComplete="organization"
-								className="mt-1 focus:border-primary focus:ring-0 focus:border-3 focus:text-xl block w-full text-center text-xl tracking-wider border-gray-light border-t-0 border-l-0 border-r-0"
+								className="mt-1 focus:border-primary focus:ring-0 focus:border-3 focus:text-xl block w-full text-center text-xl tracking-wider border-gray-dark border-t-0 border-l-0 border-r-0"
 							/>
 						</div>
 
-						<div className="p-3 w-full">
+						<div className="p-3 w-full md:w-1/2">
 							<label
 								htmlFor="email_address"
 								className="block text-md md:text-xl font-medium text-gray-darkest"
@@ -69,7 +65,7 @@ const PartnerForm = () => {
 								name="email_address"
 								id="email_address"
 								autoComplete="email"
-								className="mt-1 focus:border-primary focus:ring-0 focus:border-3 focus:text-xl block w-full text-center text-xl tracking-wider border-gray-light border-t-0 border-l-0 border-r-0"
+								className="mt-1 focus:border-primary focus:ring-0 focus:border-3 focus:text-xl block w-full text-center text-xl tracking-wider border-gray-dark border-t-0 border-l-0 border-r-0"
 							/>
 						</div>
 
@@ -85,7 +81,22 @@ const PartnerForm = () => {
 								id="phone"
 								name="phone"
 								autoComplete="phone"
-								className="mt-1 focus:border-primary focus:ring-0 focus:border-3 focus:text-xl block w-full text-center text-xl tracking-wider border-gray-light border-t-0 border-l-0 border-r-0"
+								className="mt-1 focus:border-primary focus:ring-0 focus:border-3 focus:text-xl block w-full text-center text-xl tracking-wider border-gray-dark border-t-0 border-l-0 border-r-0"
+							/>
+						</div>
+						<div className="p-3 w-full md:w-1/2">
+							<label
+								htmlFor="city"
+								className="block text-md md:text-xl font-medium text-gray-darkest"
+							>
+								City
+							</label>
+							<input
+								type="text"
+								id="city"
+								name="city"
+								autoComplete="city"
+								className="mt-1 focus:border-primary focus:ring-0 focus:border-3 focus:text-xl block w-full text-center text-xl tracking-wider border-gray-dark border-t-0 border-l-0 border-r-0"
 							/>
 						</div>
 
@@ -96,19 +107,15 @@ const PartnerForm = () => {
 							>
 								Website
 							</label>
-							{/* <div className="mt-1 flex rounded-md"> */}
-							{/* <span className="inline-flex items-center px-3 rounded-l-md border  border-primary bg-secondary text-black text-lg font-medium">
-									http://
-								</span> */}
+
 							<input
 								type="url"
 								name="company_website"
 								id="company_website"
 								autoComplete="url"
-								className="mt-1 focus:border-primary focus:ring-0 focus:border-3 focus:text-xl block w-full text-center text-xl tracking-wider border-gray-light border-t-0 border-l-0 border-r-0"
+								className="mt-1 focus:border-primary focus:ring-0 focus:border-3 focus:text-xl block w-full text-center text-xl tracking-wider border-gray-dark border-t-0 border-l-0 border-r-0"
 								placeholder="www.example.com"
 							/>
-							{/* //</div> */}
 						</div>
 
 						<div className="p-3 w-full md:w-1/2">
@@ -116,7 +123,7 @@ const PartnerForm = () => {
 								htmlFor="service"
 								className="block text-md md:text-xl font-medium text-gray-darkest"
 							>
-								Type of Service
+								Business Category
 							</label>
 							<textarea
 								required
@@ -124,41 +131,43 @@ const PartnerForm = () => {
 								name="service"
 								id="service"
 								rows="2"
-								className="mt-1 focus:border-primary focus:ring-0 focus:border-3 focus:text-xl block w-full text-center text-xl tracking-wider border-gray-light border-t-0 border-l-0 border-r-0"
+								placeholder="Photographer, Catering, etc.."
+								className="mt-1 focus:border-primary focus:ring-0 focus:border-3 focus:text-xl block w-full text-center text-xl tracking-wider border-gray-dark border-t-0 border-l-0 border-r-0"
 							></textarea>
 						</div>
 
-						<div className="p-3 w-full md:w-1/2">
-							<label
-								htmlFor="service_area"
-								className="block text-md md:text-xl font-medium text-gray-darkest"
-							>
-								Service Area
-							</label>
-							<textarea
-								required
-								type="text"
-								name="service_area"
-								id="service_area"
-								rows="2"
-								className="mt-1 focus:border-primary focus:ring-0 focus:border-3 focus:text-xl block w-full text-center text-xl tracking-wider border-gray-light border-t-0 border-l-0 border-r-0"
-							></textarea>
-						</div>
+						
 
 						<div className="p-3 w-full md:w-1/2">
 							<label
 								htmlFor="message"
 								className="block text-md md:text-xl font-medium text-gray-darkest"
 							>
-								Additional Info.
+								Additional Information
 							</label>
 							<textarea
 								type="text"
 								name="message"
 								id="message"
 								rows="2"
-								className="mt-1 focus:border-primary focus:ring-0 focus:border-3 focus:text-xl block w-full text-center text-xl tracking-wider border-gray-light border-t-0 border-l-0 border-r-0"
+								className="mt-1 focus:border-primary focus:ring-0 focus:border-3 focus:text-xl block w-full text-center text-xl tracking-wider border-gray-dark border-t-0 border-l-0 border-r-0"
 							></textarea>
+						</div>
+						<div className="p-3 w-full ">
+							<label
+								htmlFor="agreement"
+								className="block text-md md:text-xl font-medium text-gray-darkest mx-auto w-full md:w-1/2"
+							>
+								<input
+									type="checkbox"
+									name="agreement"
+									id="agreement"
+									required
+									className=" w-6 h-6 mr-3 focus:border-primary focus:ring-0 focus:border-3 focus:text-xl   text-center text-xl tracking-wider"
+								/>
+								Agree to Plannit business terms and acknowledge our privacy
+								rules.
+							</label>
 						</div>
 
 						<div className="p-3 w-full">
@@ -166,7 +175,7 @@ const PartnerForm = () => {
 								type="submit"
 								className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-primary hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"
 							>
-								Save
+								Submit
 							</button>
 						</div>
 					</form>

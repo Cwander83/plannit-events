360a5466743f56b1d25f42a0d448b4e471da8698
Chris Wander <31929558+Cwander83@users.noreply.github.com>
2021-01-20 16:22:57 -0500

    image

--- src/pages/Home/Landing.jsx
+++ src/pages/Home/Landing.jsx
@@ -1,41 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import image from '../../images/landing_image.jpg';
+import image2 from '../../images/small-2.jpg';
 
 const Landing = () => {
+	const [width, setWidth] = useState(null);
+
+	useEffect(() => {
+		setWidth({ width: window.innerWidth });
+	}, []);
 	return (
-		<div id="#" className="h-auto bg-gray-light">
+		<div id="#" className="h-auto ">
+			{/* landing title and header image */}
 			<div
-				className="sm:hidden bg-cover bg-no-repeat max-h-screen"
-				style={{ backgroundImage: `url(${image})`, height: '900px' }}
+				className="relative w-full max-h-screen h-96 shadow-md"
+				style={{
+					background: `linear-gradient(
+					rgba(0, 0, 0, 0.5),
+					rgba(0, 0, 0, 0.5)
+				  ),url(${image}) no-repeat`,
+					backgroundRepeat: 'no-repeat',
+					backgroundSize: 'cover',
+					backgroundPosition: 'center',
+				}}
 			>
-				<h1 className="mt-auto mb-0">Plannit's Event Planning Company</h1>
+				<h1 className="absolute bottom-18 left-6 md:bottom-12 md:left-14 text-xl sm:text-2xl md:text-4xl text-gray-lightest font-medium tracking-wide">
+					Plannit's Event Planning
+				</h1>
+				<h2 className="absolute bottom-4 text-xl text-secondary font-medium antialiased w-full text-center">
+					Plannit provides the convenience of creating your own event.
+				</h2>
 			</div>
-			<div
-				className="hidden sm:block bg-cover bg-no-repeat max-h-screen"
-				style={{ backgroundImage: `url(${image})`, height: '900px' }}
-			>
-				<h1 className="mt-auto mb-0">Plannit's Event Planning Company</h1>
-			</div>
-			<div className="flex flex-row text-center ">
-				<div className="w-1/2 h-5/6 py-8 my-auto flex flex-col justify-center item-center bg-secondary shadow-md rounded-r-lg">
-					<h1 className="text-black text-xl uppercase font-bold ml-20 text-left w-3/4 ">
+			{/* information and links */}
+
+			<div className="flex flex-row  w-full mt-6">
+				<div className=" flex flex-col justify-center item-center ml-10 sm:ml-12 md:ml-36">
+					<h1 className="text-black text-xl uppercase font-bold text-left">
 						Extraordinary moments are created together.
 					</h1>
-					<h2 className="text-black uppercase font-bold ml-20 text-left ">
+					<h2 className="text-black uppercase font-bold text-center">
 						Planning an event has never been easy.
 					</h2>
-					<h2 className="text-black uppercase font-bold ml-20 text-left ">
-						Plannit provides the convenience of creating your own event.
-					</h2>
-					<div className="flex flex-col justify-center items-center">
-						<button className="border-primary border-2 w-1/2 mx-auto">
-							What if Offer
-						</button>
-						<button className="border-primary border-2 w-1/2 mx-auto">
-							start building your event
-						</button>
-					</div>
 				</div>
 			</div>
 		</div>
